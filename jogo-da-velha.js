bd7736595f3532d22ded3f4feac3c70984e2047b
David Takazaki <davidmtakazaki@hotmail.com>
2019-09-09 18:32:50 -0300

    adiciona ensaio de jogo da velha

---
+++ jogo-da-velha.js
@@ -0,0 +1,27 @@
+function clickA1(){
+
+}
+function clickA2(){
+    
+}
+function clickA3(){
+    
+}
+function clickB1(){
+    
+}
+function clickB2(){
+    
+}
+function clickB3(){
+    
+}
+function clickC1(){
+    
+}
+function clickC2(){
+    
+}
+function clickC3(){
+    
+}
